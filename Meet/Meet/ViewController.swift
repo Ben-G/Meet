52685e650c21f2af65c08be2173760642199a26d
Ben-G <Benjamin.Encz@gmail.com>
2015-11-11 08:50:54 -0800

    initial commit

---
+++ Meet/Meet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meet
+//
+//  Created by Benjamin Encz on 11/10/15.
+//  Copyright © 2015 DigiTales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
