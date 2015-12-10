e2987c40aa02ef8ceb02051f8fc7048c9a1f1159
Ben-G <Benjamin.Encz@gmail.com>
2015-12-10 08:22:27 -0800

    Work without JSON Select for now

--- Meet/SwiftFlowCodableGenerator/SwiftCodableGenerator.swift
+++ Meet/SwiftFlowCodableGenerator/SwiftCodableGenerator.swift
@@ -15,46 +15,27 @@ public func generateActionConvertible(input: File) -> String {
 
     // key.kind struct, where key.inheritedtypes includes GenerateCodable
     let dictionary: XPCDictionary = structure.dictionary
-    let keypath: [String] = ["key.substructure", "key.kind"]
 
-    let result = WHERE(keypath, dictionary: dictionary, value: "struct")
+    let substructures = dictionary["key.substructure"] as? Array<XPCRepresentable>
+    let actionableStructs = substructures?.filter(filterActionConvertibleStructs)
 
-    return "\(dictionary)"
+    return "\(actionableStructs)"
 }
 
-protocol GetAnyable {
-     func getAny(key: Any) -> Any?
-}
+private func filterActionConvertibleStructs<T>(element: T) -> Bool {
+    guard let dict = element as? Dictionary<String, XPCRepresentable> else { return false }
+    guard let kind = dict["key.kind"] as? String where kind == "source.lang.swift.decl.struct" else { return false }
 
-extension Dictionary: GetAnyable {
-    func getAny(key: Any) -> Any? {
-        guard let correctKey = key as? Dictionary.Key else { return nil }
-        return self[correctKey] as Any
-    }
-}
-
-func WHERE<QueryKey: Hashable, QueryValue: Equatable, ValueType: Any>(keyPath: [QueryKey],
-    dictionary: Dictionary<QueryKey, ValueType>, value: QueryValue) -> Dictionary<QueryKey, ValueType>? {
-
-    var results: [Any] = []
-    var subdictionary: Any = dictionary
-
-    for key in keyPath {
-        if let dict = subdictionary as? GetAnyable, let value = dict.getAny(key) {
-            subdictionary = value
-            results.append(value)
-        } else if let array = subdictionary as? [GetAnyable] {
-            let getAnyable = array.flatMap { $0.getAny(key)  }
-            print("many")
-        } else {
-            return nil
+    let inheritedTypes = dict["key.inheritedtypes"] as? Array<XPCRepresentable>
+    let isActionConvertible = inheritedTypes?.filter {
+        if let value = (($0 as? Dictionary<String, XPCRepresentable>)?["key.name"]) as? String {
+            if value == "GenerateCodable" {
+                return true
+            }
         }
-    }
+        return false
+    }.count > 0
 
-    if let returnValue = subdictionary as? QueryValue where returnValue == value {
-        return results[results.endIndex - 2] as? Dictionary<QueryKey, ValueType>
-    } else {
-        return nil
-    }
-
+    return isActionConvertible
 }
+
